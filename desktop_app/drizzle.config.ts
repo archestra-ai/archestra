8cb038fd085bf0cb73e2654c86af51c2f6950a5f
Joey Orlando <joseph.t.orlando@gmail.com>
2025-09-03 10:49:07 -0400

    wip

--- desktop_app/drizzle.config.ts
+++ desktop_app/drizzle.config.ts
@@ -1,3 +1,8 @@
+/**
+ * NOTE: this config file is ONLY really here for usage with pnpm db studio
+ *
+ * Configuration for the database used by the application is defined in `desktop_app/src/backend/database/index.ts`
+ */
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
@@ -6,4 +11,7 @@ export default defineConfig({
   dialect: 'sqlite',
   // https://orm.drizzle.team/docs/sql-schema-declaration#camel-and-snake-casing
   casing: 'snake_case',
+  dbCredentials: {
+    url: `${process.env.HOME}/Library/Application Support/archestra/archestra.db`,
+  },
 });

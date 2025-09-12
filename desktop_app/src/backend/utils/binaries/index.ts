536f7ec9ec36b161e8fe45d7510cc98247ac6aad
Joey Orlando <joseph.t.orlando@gmail.com>
2025-09-12 15:27:53 -0400

    wip

--- desktop_app/src/backend/utils/binaries/index.ts
+++ desktop_app/src/backend/utils/binaries/index.ts
@@ -40,11 +40,8 @@ const getArchitecture = (): SupportedArchitecture => {
       return 'arm64';
     // this is the same as x86_64
     case 'x64':
-      // return 'x86_64';
-      // TODO: DON'T MERGE THIS! THIS IS A TEMPORARY CHANGE TO TEST SOMETHING ON WINDOWS
-      return 'arm64';
+      return 'x86_64';
     default:
-      throw new Error(`Unsupported architecture: ${arch()}`);
   }
 };
 

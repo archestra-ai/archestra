d6295400ff9b5f8a199528362832e0c1a3db6426
Ildar Iskhakov <ildar6v@gmail.com>
2025-09-17 02:03:14 +0100

    drop migrations

--- desktop_app/src/backend/database/migrations/0019_dark_puma.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `messages` ADD `prompt_tokens` integer;--> statement-breakpoint
-ALTER TABLE `messages` ADD `completion_tokens` integer;--> statement-breakpoint
-ALTER TABLE `messages` ADD `total_tokens` integer;--> statement-breakpoint
-ALTER TABLE `messages` ADD `model` text;--> statement-breakpoint
-ALTER TABLE `messages` ADD `model_context_window` integer;

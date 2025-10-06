d07d1c2f8f3d18215effe214ee0a6f40f1fac5f1
brojd <dominik@archestra.ai>
2025-10-06 12:54:18 +0200

    use tsconfigPaths plugin for vitest

--- platform/backend/vitest.config.ts
+++ platform/backend/vitest.config.ts
@@ -1,15 +1,8 @@
-import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@/config": resolve(__dirname, "./src/config.ts"),
-      "@database": resolve(__dirname, "./src/database"),
-      "@/models": resolve(__dirname, "./src/models"),
-      "@/types": resolve(__dirname, "./src/types")
-    },
-  },
+  plugins: [tsconfigPaths()],
   test: {
     globals: true,
     include: ['./src/**/*.test.ts'],

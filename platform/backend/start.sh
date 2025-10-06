415e7809f268d3e190ea5388094acce77cb3ce83
brojd <dominik@archestra.ai>
2025-10-06 22:13:12 +0200

    chore: allow running as single container

--- platform/backend/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Running migrations..."
-cd backend && pnpm exec drizzle-kit migrate --config=./drizzle.config.ts & PID=$!
-wait $PID
-
-echo "Starting server..."
-cd backend && exec node dist/server.js

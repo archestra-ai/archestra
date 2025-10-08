1276f6d2adb123505e4d886fc9455fa94c43490c
Joey Orlando <joseph.t.orlando@gmail.com>
2025-10-08 10:48:57 -0400

    remove chat model and types

--- platform/backend/src/models/index.ts
+++ platform/backend/src/models/index.ts
@@ -1,6 +1,5 @@
-export { default as AgentModel } from "./agent";
-export { default as ChatModel } from "./chat";
-export { default as InteractionModel } from "./interaction";
-export { default as ToolModel } from "./tool";
-export { default as ToolInvocationPolicyModel } from "./tool-invocation-policy";
-export { default as TrustedDataPolicyModel } from "./trusted-data-policy";
+export { default as AgentModel } from './agent';
+export { default as InteractionModel } from './interaction';
+export { default as ToolModel } from './tool';
+export { default as ToolInvocationPolicyModel } from './tool-invocation-policy';
+export { default as TrustedDataPolicyModel } from './trusted-data-policy';

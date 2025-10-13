9e07878640a9d150b0f1f8e9b61d971da291f400
jrmydev <contact@jeremymarquer.fr>
2025-10-13 20:08:37 +0200

    feat: better auth clean code config

--- platform/frontend/src/lib/config.ts
+++ platform/frontend/src/lib/config.ts
@@ -1,23 +1,23 @@
 import { env } from "next-runtime-env";
-  const envVarProxyUrl = env("NEXT_PUBLIC_ARCHESTRA_API_BASE_URL");
+  const envVarApiBaseUrl = env("NEXT_PUBLIC_ARCHESTRA_API_BASE_URL");
 
 export const getProxyUrl = (): string => {
   const proxyUrlSuffix = "/v1";
 
-  if (!envVarProxyUrl) {
+  if (!envVarApiBaseUrl) {
     return `http://localhost:9000${proxyUrlSuffix}`;
-  } else if (envVarProxyUrl.endsWith(proxyUrlSuffix)) {
-    return envVarProxyUrl;
-  } else if (envVarProxyUrl.endsWith("/")) {
-    return `${envVarProxyUrl.slice(0, -1)}${proxyUrlSuffix}`;
+  } else if (envVarApiBaseUrl.endsWith(proxyUrlSuffix)) {
+    return envVarApiBaseUrl;
+  } else if (envVarApiBaseUrl.endsWith("/")) {
+    return `${envVarApiBaseUrl.slice(0, -1)}${proxyUrlSuffix}`;
   }
-  return `${envVarProxyUrl}${proxyUrlSuffix}`;
+  return `${envVarApiBaseUrl}${proxyUrlSuffix}`;
 };
 
 export default {
   api: {
     proxyUrl: getProxyUrl(),
-    baseUrl: envVarProxyUrl || "http://localhost:9000",
+    baseUrl: envVarApiBaseUrl || "http://localhost:9000",
   },
   debug: process.env.NODE_ENV !== "production"
 };

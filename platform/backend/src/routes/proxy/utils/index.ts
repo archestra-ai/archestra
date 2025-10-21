c5353839af804ac44b0fc53fc91b660de5a23e72
Ildar Iskhakov <ildar6v@gmail.com>
2025-10-21 16:45:21 +0100

    fix: improve streaming

--- platform/backend/src/routes/proxy/utils/index.ts
+++ platform/backend/src/routes/proxy/utils/index.ts
@@ -31,6 +31,5 @@ export const persistTools = async (
 };
 
 export * as adapters from "./adapters";
-export * as streaming from "./streaming";
 export * as toolInvocation from "./tool-invocation";
 export * as trustedData from "./trusted-data";

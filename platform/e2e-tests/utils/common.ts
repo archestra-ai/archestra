2359fc684dc7564f5fd45cb505727e2edf41c1d2
Joey Orlando <joseph.t.orlando@gmail.com>
2025-11-04 15:01:10 -0500

    wip

--- platform/e2e-tests/utils/common.ts
+++ platform/e2e-tests/utils/common.ts
@@ -2,9 +2,7 @@ import { Page } from '@playwright/test';
 import { UI_BASE_URL } from '../consts';
 
 export function goToPage(page: Page, path = '') {
-  return page.goto(`${UI_BASE_URL}${path}`, {
-    waitUntil: 'domcontentloaded',
-  });
+  return page.goto(`${UI_BASE_URL}${path}`);
 }
 
 export function getRandomString(length = 10, prefix = '') {

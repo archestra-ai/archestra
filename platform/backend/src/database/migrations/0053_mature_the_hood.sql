cdef3ed23d676efa4351f546a77208be1a14f683
Anna Shishliakova <anna@archestra.ai>
2025-11-12 15:13:13 +0100

    migrations update

--- platform/backend/src/database/migrations/0053_mature_the_hood.sql
+++ platform/backend/src/database/migrations/0053_mature_the_hood.sql
@@ -1,2 +1,1 @@
-ALTER TABLE "mcp_server" ALTER COLUMN "server_type" DROP DEFAULT;--> statement-breakpoint
 ALTER TABLE "agents" ADD COLUMN "consider_context_untrusted" boolean DEFAULT false NOT NULL;

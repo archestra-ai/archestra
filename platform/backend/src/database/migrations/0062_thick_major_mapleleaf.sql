92b5287f01ca9330a045dfe38a9ad5e0bd93b749
Anna Shishliakova <anna@archestra.ai>
2025-11-25 14:35:53 +0100

    created_at in invitations

--- platform/backend/src/database/migrations/0062_thick_major_mapleleaf.sql
+++ platform/backend/src/database/migrations/0062_thick_major_mapleleaf.sql
@@ -4,4 +4,5 @@ ALTER TABLE "organization_role" ALTER COLUMN "id" DROP DEFAULT;--> statement-bre
 ALTER TABLE "organization_role" ADD COLUMN "role" text;--> statement-breakpoint
 UPDATE "organization_role" SET "role" = "name";--> statement-breakpoint
 ALTER TABLE "organization_role" ALTER COLUMN "role" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "organization_role" ADD CONSTRAINT "organization_role_organization_id_role_unique" UNIQUE("organization_id","role");
+ALTER TABLE "organization_role" ADD CONSTRAINT "organization_role_organization_id_role_unique" UNIQUE("organization_id","role");--> statement-breakpoint
+ALTER TABLE "invitation" ADD COLUMN "created_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint

4fae6ffff38733bda9b6654ff3b1e751ac7f9b42
Innokentii Konstantinov <ikonstantinov@archestra.ai>
2025-12-01 12:12:14 +0000

    bigger limit for name varchar

--- platform/backend/src/database/migrations/0071_absurd_kulan_gath.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "secret" ADD COLUMN "name" varchar(27) DEFAULT 'secret' NOT NULL;
--- platform/backend/src/database/schemas/secret.ts
+++ platform/backend/src/database/schemas/secret.ts
@@ -10,8 +10,8 @@ import type { SecretValue } from "@/types";
 
 const secretTable = pgTable("secret", {
   id: uuid("id").primaryKey().defaultRandom(),
-  /** Human-readable name to identify the secret in external storage (max 27 chars: 64 - UUID length - separator) */
-  name: varchar("name", { length: 27 }).notNull().default("secret"),
+  /** Human-readable name to identify the secret in external storage */
+  name: varchar("name", { length: 256 }).notNull().default("secret"),
   secret: jsonb("secret").$type<SecretValue>().notNull().default({}),
   /** When true, the actual secret value is stored in Vault and should be fetched using the record ID as path */
   isVault: boolean("is_vault").notNull().default(false),

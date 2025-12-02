2d1bcd3ab832273081af58ae1623a0c7bfe1f959
Anna Shishliakova <anna@archestra.ai>
2025-12-02 13:57:01 +0100

    migration fix

--- platform/backend/src/database/migrations/0074_stormy_black_panther.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "internal_mcp_catalog" ADD COLUMN "client_secret_id" uuid;--> statement-breakpoint
-ALTER TABLE "internal_mcp_catalog" ADD CONSTRAINT "internal_mcp_catalog_client_secret_id_secret_id_fk" FOREIGN KEY ("client_secret_id") REFERENCES "public"."secret"("id") ON DELETE set null ON UPDATE no action;

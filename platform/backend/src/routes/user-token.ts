301087b475b915f84d0fc35d555501a95e150228
veeradyani222 <167308256+veeradyani222@users.noreply.github.com>
2026-01-05 16:16:34 +0530

    feat: add user-token routes

---
+++ platform/backend/src/routes/user-token.ts
@@ -0,0 +1,7 @@
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+
+const userTokenRoutes: FastifyPluginAsyncZod = async (fastify) => {
+  // TODO: Implement user token routes
+};
+
+export default userTokenRoutes;

41ff73c375f8d549fe919ec0b1bb966e659cf641
Ildar Iskhakov <ildar6v@gmail.com>
2026-01-10 08:30:29 +0000

    fix: format import in theme-utils.ts to pass CI linting

--- platform/shared/themes/theme-utils.ts
+++ platform/shared/themes/theme-utils.ts
@@ -2,7 +2,11 @@
  * Theme utilities - processes tweakcn-themes.json to extract theme data
  */
 
-import { DEFAULT_THEME_ID, SUPPORTED_THEMES, type THEME_IDS } from "./theme-config";
+import {
+  DEFAULT_THEME_ID,
+  SUPPORTED_THEMES,
+  type THEME_IDS,
+} from "./theme-config";
 import themeRegistry from "./tweakcn-themes.json";
 
 // Re-export for convenience

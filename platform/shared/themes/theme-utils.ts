39519f3fc9345d14dc77e06815f3b4307487c099
Ildar Iskhakov <ildar6v@gmail.com>
2026-01-11 23:10:01 +0000

    fix: apply formatting to theme config files and openapi spec

--- platform/shared/themes/theme-utils.ts
+++ platform/shared/themes/theme-utils.ts
@@ -2,7 +2,12 @@
  * Theme utilities - processes tweakcn-themes.json to extract theme data
  */
 
-import { DARK_ONLY_THEMES, DEFAULT_THEME_ID, LIGHT_ONLY_THEMES, SUPPORTED_THEMES } from "./theme-config";
+import {
+  DARK_ONLY_THEMES,
+  DEFAULT_THEME_ID,
+  LIGHT_ONLY_THEMES,
+  SUPPORTED_THEMES,
+} from "./theme-config";
 import themeRegistry from "./tweakcn-themes.json";
 
 // Re-export for convenience

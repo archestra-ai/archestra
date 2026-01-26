beeeddd7a0e77d108a21003de28ed8357590da57
Innokentii Konstantinov <ikonstantinov@archestra.ai>
2026-01-26 18:03:40 +0000

    keep fixing tests

--- platform/e2e-tests/consts.ts
+++ platform/e2e-tests/consts.ts
@@ -75,8 +75,6 @@ export const DEFAULT_TEAM_NAME = "Default Team";
 export const ENGINEERING_TEAM_NAME = "Engineering Team";
 export const MARKETING_TEAM_NAME = "Marketing Team";
 
-export const DEFAULT_PROFILE_NAME = "Default Profile";
-
 export {
   E2eTestId,
   MCP_SERVER_TOOL_NAME_SEPARATOR,

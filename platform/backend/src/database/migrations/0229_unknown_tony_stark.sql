c2505a2727da6e5a0123393d25ce1b84cfce7e4d
Joey Orlando <joseph.t.orlando@gmail.com>
2026-05-04 09:14:18 -0400

    chore: remove stale LLM app migration

--- platform/backend/src/database/migrations/0229_unknown_tony_stark.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "interactions" ADD COLUMN "auth_method" varchar;--> statement-breakpoint
-ALTER TABLE "interactions" ADD COLUMN "authenticated_app_id" text;--> statement-breakpoint
-ALTER TABLE "interactions" ADD COLUMN "authenticated_app_name" varchar;

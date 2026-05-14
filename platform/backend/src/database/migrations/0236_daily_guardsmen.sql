f57e279b41d86ebcd1938a5866f07437dda360ea
Joey Orlando <joseph.t.orlando@gmail.com>
2026-05-14 11:54:11 -0400

    remove stale generated migrations

--- platform/backend/src/database/migrations/0236_daily_guardsmen.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "limits" ALTER COLUMN "cleanup_interval" SET DEFAULT '1w';--> statement-breakpoint
-ALTER TABLE "limits" ALTER COLUMN "cleanup_interval" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "organization" DROP COLUMN "limit_cleanup_interval";
